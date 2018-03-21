b52f4c07e3038ab2635620b79361cfca8b419ae8
Jeff Stokes <jeff@amperity.com>
2018-03-21 13:09:38 -0700

    First pass at junit result reporting

---
+++ src/greenlight/report/junit.clj
@@ -0,0 +1,56 @@
+(ns greenlight.report.junit
+  "Utilties for generating JUnit output files based on test results."
+  (:require
+    [clojure.data.xml :as xml]
+    [clojure.set :as set])
+  (:import
+    (java.time
+      Duration)))
+
+
+(defn- step->testcase
+  [ns {:greenlight.step/keys [title outcome elapsed message name]}]
+  (cond-> [:testcase (merge
+                       {:name title
+                        :classname ns}
+                       (when elapsed
+                         {:time (format "%.3f" elapsed)}))]
+    (not outcome)
+      (conj [:skipped])
+    (= :error outcome) ;; TODO: timeout?
+      (conj [:error {:message message}])
+    (= :failure outcome)
+      (conj [:failure {:message message}])))
+
+
+(defn- test->testsuite
+  [{:greenlight.test/keys [title steps outcome started-at ended-at ns]}]
+  (into
+    [:testsuite
+     (let [outcomes (frequencies (map :greenlight.step/outcome steps))]
+       {:name title
+        :timestamp (str started-at)
+        :tests (count steps)
+        :time (->> (Duration/between started-at ended-at)
+                   (.toNanos)
+                   (/ 1e9)
+                   (format "%.3f"))
+        :errors (:error outcomes 0) ;; TODO: timeout?
+        :failures (:fail outcomes 0)
+        :skipped (get outcomes nil 0)})]
+    (map (partial step->testcase ns))
+    steps))
+
+
+(defn- results->testsuites
+  [results]
+  (into
+    [:testsuites]
+    (map test->testsuite)
+    results))
+
+
+(defn report
+  "Render a set of test results to an XML string."
+  [results options]
+  (xml/emit-str (xml/sexp-as-element (results->testsuites results))))
