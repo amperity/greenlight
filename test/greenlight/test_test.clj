306a0c79ddda1b34aebbbc07cc520bc5a501510d
Jeff Stokes <jeff@amperity.com>
2018-06-20 17:50:39 -0400

    Add a greenlight test test test test..

---
+++ test/greenlight/test_test.clj
@@ -0,0 +1,36 @@
+(ns greenlight.test-test
+  (:require
+    [clojure.test :refer :all]
+    [com.stuartsierra.component :as component]
+    [greenlight.step :as step]
+    [greenlight.test :as test]))
+
+
+(test/deftest sample-greenlight-test
+  "A sample greenlight test"
+  #::step{:name 'sample-greenlight-test
+          :title "Sample greenlight test"
+          :inputs {:foo 1
+                   :bar 2
+                   :baz 3}
+          :test (fn [{:keys [foo bar baz]} ctx]
+                  (is (= 1 foo))
+                  (is (= 2 bar))
+                  (is (= 3 baz))
+                  (assoc ctx ::qux 4))}
+  #::step{:name 'another-step
+          :title "Another Step"
+          :inputs {:x (step/lookup ::qux)
+                   :y 5
+                   :z (step/component ::component)}
+          :test (fn [{:keys [x y z]} ctx]
+                  (is (= 4 x))
+                  (is (= 5 y))
+                  (is (= 6 z))
+                  ctx)})
+
+
+(deftest sample-test
+  (let [system (component/system-map ::component 6)
+        test-result (test/run-test! system (sample-greenlight-test))]
+    (is (= :pass (::test/outcome test-result)))))
