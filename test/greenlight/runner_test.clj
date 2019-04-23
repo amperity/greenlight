89277b3ba19c36ed77cf588bc3dc2eadf13edd5d
Jeff Stokes <jeff@amperity.com>
2019-04-23 19:49:25 -0400

    Fix merge conflicts with tests

--- test/greenlight/runner_test.clj
+++ test/greenlight/runner_test.clj
@@ -2,6 +2,7 @@
   (:require
     [clojure.test :refer :all]
     [greenlight.runner :as runner]
+    [greenlight.test :as test]
     [greenlight.test-suite.blue :as blue]
     [greenlight.test-suite.red :as red]))
 
@@ -31,3 +32,38 @@
       ['greenlight.test-suite.red] (mapv :greenlight.test/ns red-test-only)
       ['greenlight.test-suite.blue/sample-test] (mapv test-name one-blue-test-only)
       ['greenlight.test-suite.blue/sample-test] (mapv test-name one-blue-test-only-re))))
+
+
+(test/deftest ^:a test-a "a")
+(test/deftest ^:b test-b "b")
+(test/deftest ^:c test-c
+  {::test/group :group/c
+   ::test/description "c"})
+
+
+(deftest finding-tests
+  (let [test-vars [#'test-a #'test-b #'test-c]]
+    (are [matcher tests] (= tests (#'runner/filter-tests matcher test-vars))
+      ;; Return all with no matcher
+      nil [(test-a) (test-b) (test-c)]
+
+      ;; Match on metadata
+      :a [(test-a)]
+      :b [(test-b)]
+
+      ;; Regular expression on test name
+      #"test-a" [(test-a)]
+      #"test-." [(test-a) (test-b) (test-c)]
+
+      ;; By test properties
+      {::test/ns 'greenlight.runner-test}
+      [(test-a) (test-b) (test-c)]
+
+      {::test/ns 'greenlight.runner-test, ::test/title "test-a"}
+      [(test-a)]
+
+      {::test/group :group/c}
+      [(test-c)]
+
+      {::test/group :group/c ::test/ns 'foo.bar}
+      [])))
